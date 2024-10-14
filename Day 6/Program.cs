5eb6f7d7da242d7e88905aa55d991c2162b24091
thectogeneral <salemolorundare@gmail.com>
2024-10-14 17:09:09 +0100

    Day 6

---
+++ Day 6/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter number:");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
