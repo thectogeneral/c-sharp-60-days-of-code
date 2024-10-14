12f883f991572bef07288bc1bc08068b07081d8e
thectogeneral <salemolorundare@gmail.com>
2024-10-14 17:00:10 +0100

    day 3

---
+++ Day 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int sum = a + b;
+int difference = a - b;
+int product = a * b;
+int quotient = a / b;
+Console.WriteLine("Sum: " + sum);
