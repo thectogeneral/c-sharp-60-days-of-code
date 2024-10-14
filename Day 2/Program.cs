4bcb853aaa1bbee9e8638190ff0be97af2a004fb
thectogeneral <salemolorundare@gmail.com>
2024-10-14 16:52:40 +0100

    day 2

---
+++ Day 2/Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 25;
+double price = 19.99;
+char grade = 'A';
+string name = "John";
+bool isStudent = true;
+
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Price: " + price);
+Console.WriteLine("Grade: " + grade);
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Is Student: " + isStudent);
