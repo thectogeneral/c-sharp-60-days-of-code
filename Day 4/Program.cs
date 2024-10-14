d63d2b4f737112a945b9171fe4bfd3653a160a11
thectogeneral <salemolorundare@gmail.com>
2024-10-14 17:06:14 +0100

    update day 4

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -5,3 +5,12 @@ else if (num < 0)
     Console.WriteLine("Negative");
 else
     Console.WriteLine("Zero");
+
+
+int choice = 2;
+switch (choice)
+{
+    case 1: Console.WriteLine("Option 1"); break;
+    case 2: Console.WriteLine("Option 2"); break;
+    default: Console.WriteLine("Invalid choice"); break;
+}
