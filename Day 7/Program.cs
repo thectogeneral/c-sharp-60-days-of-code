2f68f27b0e29965cac8d1bb19384ccd6e23b62e5
thectogeneral <salemolorundare@gmail.com>
2024-10-14 17:12:12 +0100

    Day 7

---
+++ Day 7/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter operation (+, -, *, /): ");
+char op = Convert.ToChar(Console.ReadLine());
+
+double result;
+switch (op)
+{
+    case '+': result = num1 + num2; break;
+    case '-': result = num1 - num2; break;
+    case '*': result = num1 * num2; break;
+    case '/': result = num1 / num2; break;
+    default: Console.WriteLine("Invalid operator"); return;
+}
+
+Console.WriteLine("Result: " + result);
