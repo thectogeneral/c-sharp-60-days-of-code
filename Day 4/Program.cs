7f52e183d745beea7c62d71a208859f530084db0
thectogeneral <salemolorundare@gmail.com>
2024-10-14 17:05:48 +0100

    Day 5

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,4 +1,4 @@
-﻿int num = -10;
+﻿int num = 10;
 if (num > 0)
     Console.WriteLine("Positive");
 else if (num < 0)
---
+++ Day 5/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+int j = 10;
+while (j > 0)
+{
+    Console.WriteLine(j);
+    j--;
+}
