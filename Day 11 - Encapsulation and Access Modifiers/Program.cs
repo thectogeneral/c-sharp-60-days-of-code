e53dccd915d7e55a5aec72819a4d91aa0418ff57
thectogeneral <salemolorundare@gmail.com>
2024-10-21 21:11:35 +0100

    Encapsulation and access modifiers

---
+++ Day 11 - Encapsulation and Access Modifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Person
+{
+    private string _name;
+    private int _age;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value > 0)
+                _age = value;
+        }
+    }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, {Name}!");
+    }
+}
