aff822ace82fa2a161d2d5bc637dba64bd823830
thectogeneral <salemolorundare@gmail.com>
2024-10-21 21:06:52 +0100

    Constructors

---
+++ Day 10 - Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Person
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, {Name}!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("John", 30);
+        person.Greet();
+    }
+}
