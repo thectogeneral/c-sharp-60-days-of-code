6040e596676c85bd48b3311467f8252269a3b27d
thectogeneral <salemolorundare@gmail.com>
2024-10-21 20:59:21 +0100

    Methods and Properties

---
+++ Day 9 - Methods and Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, {Name}!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person { Name = "John", Age = 30 };
+        person.Greet();
+    }
+}
