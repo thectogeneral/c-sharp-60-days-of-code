32d402f6895eb1effac05aa335457063296c8e8a
thectogeneral <salemolorundare@gmail.com>
2024-10-21 20:49:48 +0100

    Classes and Objects

---
+++ Day 8/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person { Name = "John", Age = 30 };
+        person.PrintDetails();
+    }
+}
